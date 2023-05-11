b0dda860da16a24c18c08e635fe7a997259c73c3
Ganeshkumar Ayyasamy <ganeshkumaray@maveric-systems.com>
2023-05-11 22:53:58 +0530

    feat: Login smaple api request

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -6,11 +6,12 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import "./style.css";
+import axios from 'axios';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -45,68 +46,79 @@ function a11yProps(index: number) {
   };
 }
 
-export default function BasicTabs() {
 
-
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-    </Box>
-  );
-}
 export default function Login({modalOpen, closeModal}) {
   const [open, setOpen] = React.useState(true);
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-       setValue(newValue);
-     };
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleInputChange = (e, setValue) => {
+    setValue( e.currentTarget.value );
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
   };
+
+  const handleSubmitLogin = async () => {
+    const requestBody = {
+      username: email,
+      password: password
+    };
+
+    await axios.post('http://192.168.0.5:3010/user/login', requestBody)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 
   return (
     <div>
-      <Dialog open={modalOpen} onClose={closeModal}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+      <Dialog className={'login-form-dialog'} open={modalOpen} onClose={closeModal}>
+        {/* <DialogTitle>Subscribe</DialogTitle> */}
+        <DialogContent>          
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="Login" {...a11yProps(0)} />
+                <Tab label="Registration" {...a11yProps(1)} />
+              </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Email"
+                type="email"
+                fullWidth
+                variant="standard"
+                onChange={(e)=> handleInputChange(e, setEmail)}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Password"
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={(e)=> handleInputChange(e, setPassword)}
+              />
+              <Button type="button" color="primary" className="form__custom-button-login" onClick={handleSubmitLogin}>
+                Log in
+              </Button>
+            </TabPanel>
+            
+            <TabPanel value={value} index={1}>
+              Item Two
+            </TabPanel>
+          </Box>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={closeModal}>Cancel</Button>
-          <Button onClick={closeModal}>Subscribe</Button>
-        </DialogActions>
       </Dialog>
     </div>
   );
